4183cddc7eb065a463259b1563e5181951716201
Atharva Jakhetiya <jakhetiyaatharva@gmail.com>
2025-01-02 20:43:27 +0530

    first commit

--- main.py
+++ main.py
@@ -27,4 +27,31 @@ class Car:
         
         self.speed_set = False
         
-        self.center = [self.position[0]]
+        self.center = [self.position[0] + (CAR_SIZE_X/2), self.position[1] + (CAR_SIZE_Y/2)]
+        
+        self.radars = []
+        self.drawing_radars = []
+        
+        self.alive = True
+        
+        self.distance = 0
+        self.time = 0
+        
+    def draw(self, screen):
+        screen.blit(self.rotated_sprite, self.position)
+        self.draw_radars(screen)
+        
+    def draw_radars(self, screen):
+        for radar in self.radars:
+            position = radar[0]
+            pygame.draw.line(screen, (0, 255, 0), self.center, position, 1)
+            pygame.draw.circle(screen, (0,2555, 0), position, 5)
+            
+    def check_collision(self, game_map):
+        self.alive = True
+        for point in self.course:
+            if game_map.get_at((int(point[0]), int(point[1]))) == BORDER_COLOR:
+                self.alive = False
+                break
+    
+    
